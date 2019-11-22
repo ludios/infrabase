ludios/infrabase

commit 780c11ddfc1235722f921da50f9bad5c58457509

Replace Diesel with rust-postgres now that we've become a stored procedure database cultist

schema/up.sql
@@ -1,0 +1,131 @@
+CREATE DOMAIN inet4          AS inet         CHECK (family(VALUE) = 4);
+CREATE DOMAIN hostname       AS varchar(32)  CHECK (VALUE ~ '\A[-_a-z0-9]+\Z');
+CREATE DOMAIN netname        AS varchar(32)  CHECK (VALUE ~ '\A(NONE|[-_a-z0-9]+)\Z');
+CREATE DOMAIN port           AS integer      CHECK (VALUE > 0 AND VALUE <= 65536);
+CREATE DOMAIN wireguard_key  AS varchar(44)  CHECK (VALUE ~ '\A[+/A-Za-z0-9]{43}=\Z');
+ -- Match default /etc/adduser.conf NAME_REGEX
+CREATE DOMAIN username       AS varchar(32)  CHECK (VALUE ~ '\A[a-z][-a-z0-9_]{1,31}\Z');
+CREATE DOMAIN email          AS varchar(254) CHECK (VALUE ~ '\A.+@.+\Z');
+CREATE DOMAIN owner          AS varchar(32);
+
+-- INSERT name='NONE' to support machines that have no addresses in machine_addresses
+CREATE TABLE networks (
+    name  netname NOT NULL PRIMARY KEY
+);
+
+-- If network `name` can reach all addresses on `other_network`, it must be listed here
+-- Network must also have a self-link if machines on the network can reach other addresses on the network
+--
+-- priority decides which endpoint should be used when there are multiple candidates
+--
+-- INSERT (name='NONE', other_network='internet', priority=...) to indicate that machine
+-- without any addresses in machine_addresses can reach machines on network 'internet'.
+--
+-- (internet,  internet,   0)
+-- (homelan,   homelan,   -1) <- if machine on homelan reaching another machine on homelan, prefer this over (internet, internet) or (homelan, internet)
+-- (homelan,   internet,   0)
+-- (work,      work,      -1)
+-- (work,      internet,   0)
+--
+CREATE TABLE network_links (
+    name           netname  NOT NULL REFERENCES networks(name),
+    other_network  netname  NOT NULL REFERENCES networks(name),
+    priority       integer  NOT NULL,
+    PRIMARY KEY (name, other_network)
+);
+
+-- Hosting accounts
+CREATE TABLE providers (
+    id     serial       NOT NULL PRIMARY KEY,
+    name   varchar(32)  NOT NULL,
+    email  email        NOT NULL
+);
+
+-- Valid owners
+CREATE TABLE owners (
+    owner  owner  NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE machines (
+    hostname            hostname                  NOT NULL PRIMARY KEY,
+    added_time          timestamp with time zone  NOT NULL DEFAULT now(),
+    owner               owner                     NOT NULL REFERENCES owners(owner),
+    provider_id         integer                   REFERENCES providers(id),
+    provider_reference  text
+);
+
+-- Separate table because not all machines have an infrabase-managed WireGuard interface
+CREATE TABLE wireguard_interfaces (
+   hostname           hostname       NOT NULL PRIMARY KEY REFERENCES machines,
+   wireguard_ip       inet4          NOT NULL,
+   wireguard_port     port           NOT NULL,
+   wireguard_privkey  wireguard_key  NOT NULL,
+   wireguard_pubkey   wireguard_key  NOT NULL,
+   UNIQUE (wireguard_privkey),
+   UNIQUE (wireguard_pubkey)
+);
+
+-- Separate table because not all machines have an SSH server
+CREATE TABLE ssh_servers (
+    hostname  hostname  NOT NULL PRIMARY KEY REFERENCES machines,
+    ssh_port  port      NOT NULL,
+    ssh_user  username  NOT NULL DEFAULT 'root'
+);
+
+CREATE TABLE wireguard_keepalives (
+    source_machine  hostname  NOT NULL REFERENCES machines(hostname),
+    target_machine  hostname  NOT NULL REFERENCES machines(hostname),
+    -- `man wg` says "PersistentKeepalive — a seconds interval, between 1 and 65535 inclusive"
+    interval_sec    integer   NOT NULL CHECK (interval_sec >= 1 AND interval_sec <= 65535),
+    PRIMARY KEY (source_machine, target_machine)
+);
+
+-- Note: you should use a different WireGuard port for each machine behind the same NAT.
+--
+-- WireGuard remembers just one endpoint per machine and if it gets a packet from IP:904
+-- it will assume (IP, 904) is reachable even if the port forward on the router is 905 -> 904
+-- and the endpoint was originally configured to use :905
+
+CREATE TABLE machine_addresses (
+    hostname        hostname  NOT NULL REFERENCES machines,
+    network         netname   NOT NULL REFERENCES networks(name),
+    address         inet      NOT NULL,
+    ssh_port        port,
+    wireguard_port  port,
+    PRIMARY KEY (hostname, network, address),
+    UNIQUE (address, ssh_port),
+    UNIQUE (address, wireguard_port)
+);
+
+CREATE VIEW machines_view AS
+    SELECT
+        machines.hostname,
+        added_time,
+        owner,
+        provider_id,
+        providers.name AS provider_name,
+        providers.email AS provider_email,
+        provider_reference,
+        coalesce(networks.networks, array[]::varchar[]) AS networks,
+        wireguard_ip,
+        wireguard_port,
+        wireguard_privkey,
+        wireguard_pubkey,
+        ssh_port,
+        ssh_user
+    FROM machines
+    LEFT JOIN wireguard_interfaces ON machines.hostname = wireguard_interfaces.hostname
+    LEFT JOIN ssh_servers          ON machines.hostname = ssh_servers.hostname
+    LEFT JOIN providers            ON machines.provider_id = providers.id
+    LEFT JOIN (SELECT hostname, array_agg(network::varchar) AS networks FROM machine_addresses GROUP BY hostname) networks ON machines.hostname = networks.hostname;
+
+-- Remove all mentions of machine from database
+CREATE PROCEDURE remove_machine(kill_hostname varchar)
+LANGUAGE plpgsql
+AS $$
+    DELETE FROM wireguard_interfaces WHERE hostname = kill_hostname;
+    DELETE FROM ssh_servers          WHERE hostname = kill_hostname;
+    DELETE FROM machine_addresses    WHERE hostname = kill_hostname;
+    DELETE FROM wireguard_keepalives WHERE source_machine = kill_hostname OR target_machine = kill_hostname;
+    DELETE FROM machines             WHERE hostname = kill_hostname;
+$$;

migrations/00000000000000_diesel_initial_setup/up.sql
@@ -1,36 +1,0 @@
--- This file was automatically created by Diesel to setup helper functions
--- and other internal bookkeeping. This file is safe to edit, any future
--- changes will be added to existing projects as new migrations.
-
-
-
-
--- Sets up a trigger for the given table to automatically set a column called
--- `updated_at` whenever the row is modified (unless `updated_at` was included
--- in the modified columns)
---
--- # Example
---
--- ```sql
--- CREATE TABLE users (id SERIAL PRIMARY KEY, updated_at TIMESTAMP NOT NULL DEFAULT NOW());
---
--- SELECT diesel_manage_updated_at('users');
--- ```
-CREATE OR REPLACE FUNCTION diesel_manage_updated_at(_tbl regclass) RETURNS VOID AS $$
-BEGIN
-    EXECUTE format('CREATE TRIGGER set_updated_at BEFORE UPDATE ON %s
-                    FOR EACH ROW EXECUTE PROCEDURE diesel_set_updated_at()', _tbl);
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION diesel_set_updated_at() RETURNS trigger AS $$
-BEGIN
-    IF (
-        NEW IS DISTINCT FROM OLD AND
-        NEW.updated_at IS NOT DISTINCT FROM OLD.updated_at
-    ) THEN
-        NEW.updated_at := current_timestamp;
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;

migrations/2019-07-20-012659_create_machines/up.sql
@@ -1,89 +1,0 @@
-CREATE DOMAIN hostname       AS varchar(32)  CHECK (VALUE ~ '\A[-_a-z0-9]+\Z');
-CREATE DOMAIN netname        AS varchar(32)  CHECK (VALUE ~ '\A(NONE|[-_a-z0-9]+)\Z');
-CREATE DOMAIN port           AS integer      CHECK (VALUE > 0 AND VALUE <= 65536);
-CREATE DOMAIN wireguard_key  AS varchar(44)  CHECK (VALUE ~ '\A[+/A-Za-z0-9]{43}=\Z');
- -- Match default /etc/adduser.conf NAME_REGEX
-CREATE DOMAIN username       AS varchar(32)  CHECK (VALUE ~ '\A[a-z][-a-z0-9_]{1,31}\Z');
-CREATE DOMAIN email          AS varchar(254) CHECK (VALUE ~ '\A.+@.+\Z');
-CREATE DOMAIN owner          AS varchar(32);
-
--- INSERT name='NONE' to support machines that have no addresses in machine_addresses
-CREATE TABLE networks (
-    name  netname NOT NULL PRIMARY KEY
-);
-
--- If network `name` can reach all addresses on `other_network`, it must be listed here
--- Network must also have a self-link if machines on the network can reach other addresses on the network
---
--- priority decides which endpoint should be used when there are multiple candidates
---
--- INSERT (name='NONE', other_network='internet', priority=...) to indicate that machine
--- without any addresses in machine_addresses can reach machines on network 'internet'.
---
--- (internet,  internet,   0)
--- (homelan,   homelan,   -1) <- if machine on homelan reaching another machine on homelan, prefer this over (internet, internet) or (homelan, internet)
--- (homelan,   internet,   0)
--- (work,      work,      -1)
--- (work,      internet,   0)
---
-CREATE TABLE network_links (
-    name           netname NOT NULL REFERENCES networks(name),
-    other_network  netname NOT NULL REFERENCES networks(name),
-    priority       integer NOT NULL,
-    PRIMARY KEY (name, other_network)
-);
-
--- hosting accounts
-CREATE TABLE providers (
-    id     serial      NOT NULL PRIMARY KEY,
-    name   varchar(32) NOT NULL,
-    email  email       NOT NULL
-);
-
--- who owns the machine?
-CREATE TABLE owners (
-    owner  owner NOT NULL PRIMARY KEY
-);
-
-CREATE TABLE machines (
-    hostname           hostname                 NOT NULL PRIMARY KEY,
-    wireguard_ip       inet,
-    wireguard_port     port                     CHECK ((wireguard_ip IS NOT NULL AND wireguard_port    IS NOT NULL) OR (wireguard_ip IS NULL AND wireguard_port    IS NULL)),
-    wireguard_privkey  wireguard_key            CHECK ((wireguard_ip IS NOT NULL AND wireguard_privkey IS NOT NULL) OR (wireguard_ip IS NULL AND wireguard_privkey IS NULL)),
-    wireguard_pubkey   wireguard_key,           CHECK ((wireguard_ip IS NOT NULL AND wireguard_pubkey  IS NOT NULL) OR (wireguard_ip IS NULL AND wireguard_pubkey  IS NULL)),
-    ssh_port           port,
-    ssh_user           username                 DEFAULT 'root' CHECK ((ssh_port IS NOT NULL AND ssh_user IS NOT NULL) OR (ssh_port IS NULL AND ssh_user IS NULL)),
-    added_time         timestamp with time zone NOT NULL DEFAULT now(),
-    owner              owner                    NOT NULL REFERENCES owners(owner),
-    provider_id        integer                  REFERENCES providers(id),
-    provider_reference text,
-
-    UNIQUE (wireguard_ip),
-    UNIQUE (wireguard_privkey),
-    UNIQUE (wireguard_pubkey)
-);
-
-CREATE TABLE wireguard_keepalives (
-    source_machine hostname NOT NULL REFERENCES machines(hostname),
-    target_machine hostname NOT NULL REFERENCES machines(hostname),
-    -- `man wg` says "PersistentKeepalive — a seconds interval, between 1 and 65535 inclusive"
-    interval_sec   integer  NOT NULL CHECK (interval_sec >= 1 AND interval_sec <= 65535),
-    PRIMARY KEY (source_machine, target_machine)
-);
-
--- Note: you should use a different WireGuard port for each machine behind the same NAT.
---
--- WireGuard remembers just one endpoint per machine and if it gets a packet from IP:904
--- it will assume (IP, 904) is reachable even if the port forward on the router is 905 -> 904
--- and the endpoint was originally configured to use :905
-
-CREATE TABLE machine_addresses (
-    hostname       hostname NOT NULL REFERENCES machines,
-    network        netname  NOT NULL REFERENCES networks(name),
-    address        inet     NOT NULL,
-    ssh_port       port,
-    wireguard_port port,
-    PRIMARY KEY (hostname, network, address),
-    UNIQUE (address, ssh_port),
-    UNIQUE (address, wireguard_port)
-);

Cargo.toml
@@ -8,8 +8,6 @@ edition = "2018"
 
 [dependencies]
 snafu = "0.5.0"
-diesel = { version = "1.0.0", features = ["postgres", "chrono", "network-address"] }
-ipnetwork = "0.15.0"
 dirs = "2.0"
 dotenv = "0.9.0"
 chrono = "0.4.7"
@@ -23,3 +21,5 @@ natural_sort = { git = "https://github.com/ludios/natural-sort-rs" }
 tabwriter = "1.1.0"
 termcolor = "1.0.5"
 runtime-fmt = "0.4.1"
+postgres = { git = "https://github.com/sfackler/rust-postgres", features = ["with-chrono-0_4"] }
+tokio-postgres = { git = "https://github.com/sfackler/rust-postgres" }

migrations/00000000000000_diesel_initial_setup/down.sql
@@ -1,6 +1,0 @@
--- This file was automatically created by Diesel to setup helper functions
--- and other internal bookkeeping. This file is safe to edit, any future
--- changes will be added to existing projects as new migrations.
-
-DROP FUNCTION IF EXISTS diesel_manage_updated_at(_tbl regclass);
-DROP FUNCTION IF EXISTS diesel_set_updated_at();

schema/down.sql
@@ -1,3 +1,9 @@
+DROP PROCEDURE remove_machine;
+
+DROP VIEW machines_view;
+
+DROP TABLE ssh_servers;
+DROP TABLE wireguard_interfaces;
 DROP TABLE wireguard_keepalives;
 DROP TABLE machine_addresses;
 DROP TABLE machines;

src/models.rs
@@ -1,13 +1,10 @@
-use ipnetwork::IpNetwork;
 use chrono::{DateTime, Utc};
-use crate::schema::*;
+use std::net::{IpAddr, Ipv4Addr};
 
-#[derive(Identifiable, Queryable, Debug)]
-#[primary_key(hostname)]
-#[table_name = "machines"]
+#[derive(Debug)]
 pub struct Machine {
     pub hostname: String,
-    pub wireguard_ip: Option<IpNetwork>,
+    pub wireguard_ip: Option<Ipv4Addr>,
     pub wireguard_port: Option<i32>,
     pub wireguard_privkey: Option<String>,
     pub wireguard_pubkey: Option<String>,
@@ -17,63 +14,39 @@ pub struct Machine {
     pub owner: String,
     pub provider_id: Option<i32>,
     pub provider_reference: Option<String>,
+    pub networks: Vec<String>,
+    pub addresses: Vec<MachineAddress>,
 }
 
-#[derive(Insertable, Debug)]
-#[table_name = "machines"]
-pub struct NewMachine {
-    pub hostname: String,
-    pub wireguard_ip: Option<IpNetwork>,
-    pub wireguard_port: Option<i32>,
-    pub wireguard_privkey: Option<String>,
-    pub wireguard_pubkey: Option<String>,
-    pub ssh_port: Option<i32>,
-    pub ssh_user: Option<String>,
-    pub owner: String,
-    pub provider_id: Option<i32>,
-    pub provider_reference: Option<String>,
-}
-
-#[derive(Identifiable, Insertable, Queryable, Associations, Debug)]
-#[primary_key(hostname, network, address)]
-#[belongs_to(Machine, foreign_key = "hostname")]
-#[table_name = "machine_addresses"]
+#[derive(Debug)]
 pub struct MachineAddress {
     pub hostname: String,
     pub network: String,
-    pub address: IpNetwork,
+    pub address: IpAddr,
     pub ssh_port: Option<i32>,
     pub wireguard_port: Option<i32>,
 }
 
-#[derive(Identifiable, Insertable, Queryable, Associations, Debug)]
-#[primary_key(source_machine, target_machine)]
-#[belongs_to(Machine, foreign_key = "source_machine")]
-#[table_name = "wireguard_keepalives"]
+#[derive(Debug)]
 pub struct WireguardKeepalive {
     pub source_machine: String,
     pub target_machine: String,
     pub interval_sec: i32,
 }
 
-#[derive(Identifiable, Queryable, Debug)]
-#[primary_key(name)]
-#[table_name = "networks"]
+#[derive(Debug)]
 pub struct Network {
     pub name: String,
 }
 
-#[derive(Identifiable, Queryable, Debug)]
-#[primary_key(name, other_network, priority)]
-#[table_name = "networks"]
+#[derive(Debug)]
 pub struct NetworkLink {
     pub name: String,
     pub other_network: String,
     pub priority: i32,
 }
 
-#[derive(Identifiable, Queryable, Debug)]
-#[table_name = "providers"]
+#[derive(Debug)]
 pub struct Provider {
     pub id: i32,
     pub name: String,

commit 72104df95a4996ea2d9145a247e0dccbefb05343

Fix regression introduced in 780c11ddfc1235722f921da50f9bad5c58457509: machines with no networks should get assigned the "NONE" network (typically configured to be able to reach "internet" addresses).
Previously fixed in 5432ca446329b4fa31c2820ceefbc3fe9428fd62

schema/up.sql
@@ -106,7 +106,7 @@ CREATE VIEW machines_view AS
         providers.name AS provider_name,
         providers.email AS provider_email,
         provider_reference,
-        coalesce(networks.networks, array[]::varchar[]) AS networks,
+        coalesce(networks.networks, ARRAY['NONE']) AS networks,
         wireguard_ip,
         wireguard_port,
         wireguard_privkey,